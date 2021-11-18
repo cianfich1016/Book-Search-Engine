c5aa8beba0bf7cc34c50edea5f5c25c298b16822
Rachel Cianfichi <cianfich@gmail.com>
2021-11-18 16:24:32 -0500

    Created index.js and typeDefs.js in schemas.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String  
+    description: String
+    title: String
+    image: String
+    link: String
+    authors: [String]
+  }
+  input saveBook {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+  }
+  
+  type Query {
+    me: User
+}
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, Password: String!): Auth
+    saveBook(input: saveBook): User
+    removeBook: (bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
