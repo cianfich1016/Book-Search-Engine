9aba0cca02d87e576603e8f78fd7438b08b475b9
Rachel Cianfichi <cianfich@gmail.com>
2021-11-20 18:16:08 -0500

    debug signup form issue

--- server/server.js
+++ server/server.js
@@ -21,27 +21,27 @@ server.start().then(res => {
   server.applyMiddleware({ app});
 
   app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+  app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`)
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
-});
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 
 
 
 });
-//Update Express.js to use Apollo server features
+
 
 
