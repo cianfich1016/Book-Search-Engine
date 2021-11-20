c1fa58a175418b4b7ece6d8ec60fac06a2008f56
Rachel Cianfichi <cianfich@gmail.com>
2021-11-19 20:54:09 -0500

    debug graphql and finish pages as well as connect to heroku.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-const {gql} = require('apollo-server-express')
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
@@ -40,7 +40,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: saveBook): User
-    removeBook: (bookId: String!): User
+    removeBook(bookId: String!): User
   }
 `;
 
